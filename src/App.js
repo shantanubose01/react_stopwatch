f3c66ea3b6fb8907cb8bf94943e331ac13565e74
shantanubose01 <sbose1918@gmail.com>
2023-03-08 19:23:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
+import {useState,useEffect} from 'react';
+export default function App() {
+ 
+  const [isRunning, setRunning] = useState(false);
+  const [timer, setTimer ] = useState(0);
+  const handlePause = ()=>{
+     setRunning(false);
+  }
+  const handleStart = ()=>{
+    setRunning(true);
+  }
+  const handleReset = ()=>{
+    setTimer(0);
+    setRunning(false);
+  }
+  useEffect(()=>{
+    let interval ;
+    if(isRunning===true){
+          interval = setInterval(()=>{
+          
+          setTimer(prev=>prev+1);
+         
+         },10)
+        }
+         
+         return ()=>clearInterval(interval) ;
+    
+  },[isRunning])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <p style={{fontSize : '50px'}}>  {Math.floor(timer/6000)} : {Math.floor(timer/100)%60} : {timer%100} </p>
+    {isRunning ? <button onClick={handlePause}>Pause</button> :<button onClick={handleStart}>Start</button>}
+    <button disabled={!isRunning && timer===0} style={{marginLeft : '20px'}} onClick ={handleReset}>Reset</button>
     </div>
   );
 }
-
-export default App;
